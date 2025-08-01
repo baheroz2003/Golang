c3783976fb16e79d460f116fa85f81eda00433b4
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-08-01 10:22:43 +0530

    Update main.go

--- commaok/main.go
+++ commaok/main.go
@@ -1,19 +1,29 @@
-// main2.go
-
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"bufio"  // Buffered I/O package, line-by-line input lene ke liye
+	"fmt"    // Formatting package, print karne ke liye
+	"os"     // OS package, input/output stream ke liye
 )
 
 func main() {
+	// Welcome message print karega
 	fmt.Println("Welcome to input")
+
+	// Reader create kar rahe hain jo standard input (keyboard) se line read karega
 	reader := bufio.NewReader(os.Stdin)
+
+	// Prompt message user ke liye
 	fmt.Println("Enter the rating for our pizza:")
 
-	input, _ := reader.ReadString('\n')
-	fmt.Println("Thanks for rating,", input)
-	//see the err not equal to nil wala
+	// User se ek line input le rahe hain (jab tak Enter press na ho)
+	input, err := reader.ReadString('\n')
+
+	// Agar koi error aayi input lene mein toh print karo
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+	} else {
+		// Agar input sahi mila toh user ko thank you bolenge
+		fmt.Println("Thanks for rating,", input)
+	}
 }
