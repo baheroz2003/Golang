110708bea25ec346fc9daf81450ba424bc014787
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-08-01 12:58:42 +0530

    Update main.go

--- timego/main.go
+++ timego/main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to time study of golang")
-	presenttime := time.Now()
-	fmt.Println(presenttime)
-	fmt.Println(presenttime.Format("01-02-2006 15:04:05 Monday"))
+	now := time.Now()
+	fmt.Println("Full timestamp:", now)
+	fmt.Println("Milliseconds:", now.UnixMilli())
+	fmt.Println("Nanoseconds:", now.UnixNano())
+	fmt.Println(now)
+	fmt.Println(now.Format("01-02-2006 15:04:05 Monday"))
 	createdDate := time.Date(2020, time.August, 10, 23, 23, 0, 0, time.UTC)
 	fmt.Println(createdDate.Format("01-02-2006 Monday"))
 }
